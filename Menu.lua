e0bb09decb3c12a825906c129a28bedbc2905108
Predicate <predicate.dev@gmail.com>
2014-02-24 10:20:53 -0800

    Prettify menu a bit.

--- Menu.lua
+++ Menu.lua
@@ -4,13 +4,13 @@ do
 	setfenv(1, setmetatable(namespace, { __index = _G }))
 end
 
-local MENUITEMSIZE, PADDING = 16, 5
+local MENUITEMSIZE, PADDING, MAXWIDTH = 20, 6, panel:GetWidth() / 6
 
 menu = CreateFrame("Button", nil, panel)
 menu:SetFrameStrata("HIGH")
-menu:SetHeight(32)
-menu:SetWidth(32)
-menu:SetPoint("LEFT")
+menu:SetHeight(PANELSIZE * 2)
+menu:SetWidth(PANELSIZE * 2)
+menu:SetPoint("LEFT", PADDING, 0)
 local tex = menu:CreateTexture()
 local pushedTex = menu:CreateTexture()
 tex:SetAllPoints()
@@ -37,9 +37,8 @@ local menucontainer = CreateFrame("Frame", nil, menu)
 menucontainer:Hide()
 menucontainer:SetFrameStrata("MEDIUM")
 menucontainer:SetBackdrop({ bgFile = [[Interface\BUTTONS\White8x8]] })
-menucontainer:SetBackdropColor(0, 0, 0, 0.8)
-menucontainer:SetPoint("BOTTOMLEFT", panel, "TOPLEFT", 0, 1)
-
+menucontainer:SetBackdropColor(0, 0, 0, 0.5)
+menucontainer:SetPoint("BOTTOMLEFT", menu, "TOPLEFT", 0, -1*PANELSIZE/2)
 
 local buttons, buttonorder = {}, {}
 
@@ -51,16 +50,16 @@ local function toggleMenu()
 	else
 		if needSort then
 			table.sort(buttonorder)
-			menucontainer:SetHeight(#buttonorder * (MENUITEMSIZE + PADDING) + PADDING)
+			menucontainer:SetHeight(#buttonorder * (MENUITEMSIZE + PADDING) + PADDING*2)
 			local maxwidth = 0
 			for i, name in ipairs(buttonorder) do
 				buttons[name]:ClearAllPoints()
-				buttons[name]:SetPoint("TOPLEFT", menucontainer, PADDING, -1*(MENUITEMSIZE + PADDING) * (i - 1) - PADDING)
-				buttons[name]:SetPoint("TOPRIGHT", menucontainer, -1*PADDING, -1 * (MENUITEMSIZE + PADDING) * (i - 1) - PADDING)
+				buttons[name]:SetPoint("TOPLEFT", menucontainer, 0, -1*(MENUITEMSIZE + PADDING) * (i - 1) - PADDING)
+				buttons[name]:SetPoint("TOPRIGHT", menucontainer, 0, -1 * (MENUITEMSIZE + PADDING) * (i - 1) - PADDING)
 				maxwidth = math.max(maxwidth, buttons[name].label:GetStringWidth())
 			end
 			if maxwidth ~= 0 then
-				menucontainer:SetWidth(math.min(maxwidth + MENUITEMSIZE + 3*PADDING, 300))
+				menucontainer:SetWidth(math.min(maxwidth + MENUITEMSIZE + 5*PADDING, MAXWIDTH))
 			else --hack for blizz bug
 				menucontainer:SetWidth(150)
 				local f = CreateFrame("Frame")
@@ -69,7 +68,7 @@ local function toggleMenu()
 					for _, name in ipairs(buttonorder) do
 						maxwidth = math.max(maxwidth, buttons[name].label:GetStringWidth())
 					end
-					menucontainer:SetWidth(math.min(maxwidth + MENUITEMSIZE + 3*PADDING, 300))
+					menucontainer:SetWidth(math.min(maxwidth + MENUITEMSIZE + 5*PADDING, MAXWIDTH))
 					self:SetScript("OnUpdate", nil)
 				end)
 			end
@@ -82,6 +81,28 @@ local function toggleMenu()
 end
 menu:SetScript("OnClick", toggleMenu)
 
+local hidetimer = 0
+local function timerOnUpdate(_, elapsed)
+	hidetimer = hidetimer + elapsed
+	if hidetimer > 3 then
+		toggleMenu()
+		menu:SetScript("OnUpdate", nil)
+	end
+end
+local function timerOnEnter()
+	menu:SetScript("OnUpdate", nil)
+end
+local function timerOnLeave()
+	if menucontainer:IsShown() then
+		hidetimer = 0
+		menu:SetScript("OnUpdate", timerOnUpdate)
+	end
+end
+menu:SetScript("OnEnter", timerOnEnter)
+menu:SetScript("OnLeave", timerOnLeave)
+menucontainer:SetScript("OnEnter", timerOnEnter)
+menucontainer:SetScript("OnLeave", timerOnLeave)
+
 local function _IconChanged(self, _, _, _, iconpath) self.icon:SetTexture(iconpath) end
 local function _LabelChanged(self, _, _, _, newlabel)
 	self:SetText(newlabel)
@@ -91,15 +112,21 @@ local function _DescChanged(self, _, _, _, newdesc) self.desc = newdesc end
 local function _OnClickChanged(self, _, _, _, onclick) self.OnClick = onclick end
 
 local function _OnEnter(self)
-	if self.desc then
-		GameTooltip:SetOwner(self)
-		GameTooltip:ClearLines()
-		GameTooltip:AddLine(self.name, 1, 1, 1)
-		GameTooltip:AddLine(self.desc)
-		GameTooltip:Show()
+	timerOnEnter()
+	GameTooltip:SetOwner(self)
+	GameTooltip:ClearLines()
+	if self.desc or self.label:GetStringWidth() > self.label:GetWidth() then
+		GameTooltip:AddLine(self:GetText(), 1, 1, 1)
 	end
+	if self.desc then
+		GameTooltip:AddLine(self.desc)
+	end
+	GameTooltip:Show()
 end
-local function _OnLeave(self) GameTooltip:Hide() end
+local function _OnLeave(self)
+	timerOnLeave()
+	GameTooltip:Hide()
+end
 local function _OnClick(self, ...)
 	toggleMenu()
 	if self.OnClick then self.OnClick(...) end
@@ -108,19 +135,23 @@ end
 local function addMenuItem(name, iconpath, dobj)
 	if buttons[name] then return end
 	local b = CreateFrame("Button", nil, menucontainer)
-	b:SetHeight(MENUITEMSIZE)
+	b:SetHeight(MENUITEMSIZE + PADDING)
+	b:SetHighlightTexture([[Interface/QUESTFRAME/UI-QuestLogTitleHighlight]])
+	b:GetHighlightTexture():SetVertexColor(1,1,1,0.3)
 	local icon = b:CreateTexture()
 	b.icon = icon
 	icon:SetHeight(MENUITEMSIZE)
 	icon:SetWidth(MENUITEMSIZE)
-	icon:SetPoint("LEFT")
+	icon:SetPoint("LEFT", PADDING*2, 0)
 	icon:SetTexture(iconpath)
 
 	local label = b:CreateFontString()
 	b.label = label
 	label:SetPoint("LEFT", icon, "RIGHT", PADDING, 0)
+	label:SetPoint("RIGHT", -1*PADDING, 0)
 	label:SetJustifyH("LEFT")
-	label:SetFont([[Fonts/ARIALN.TTF]], MENUITEMSIZE)
+	label:SetFont([[Fonts/ARIALN.TTF]], MENUITEMSIZE * .75)
+	label:SetShadowOffset(1, -1)
 	label:SetText(name)
 	b:SetFontString(label)
 
@@ -154,8 +185,7 @@ local function removeMenuItem(name)
 	end
 end
 
-local function newDataObject(_, name, dobj)
-	if dobj.type ~= "launcher" then return end
+local function newDataObject(name, dobj)
 	local frame = addMenuItem(name, dobj.icon, dobj)
 	DataRegistry.RegisterCallback(frame, "DataRegistry_AttributeChanged_"..name.."_icon", "IconChanged")
 	DataRegistry.RegisterCallback(frame, "DataRegistry_AttributeChanged_"..name.."_label", "LabelChanged")
@@ -164,14 +194,16 @@ local function newDataObject(_, name, dobj)
 end
 
 for name, dobj in DataRegistry.DataObjectIterator() do
-	if dobj.type == "launcher" then newDataObject(nil, name, dobj) end
+	if dobj.type == "launcher" then newDataObject(name, dobj) end
 end
-DataRegistry.RegisterCallback(menucontainer, "DataRegistry_DataObjectCreated", newDataObject)
+DataRegistry.RegisterCallback(menucontainer, "DataRegistry_DataObjectCreated", function(_, name, dobj)
+	if dobj.type == "launcher" then newDataObject(name, dobj) end
+end)
 DataRegistry.RegisterCallback(menucontainer, "DataRegistry_AttributeChanged__type", function(_, name, _, v, dobj)
 	if buttons[name] then
 		removeMenuItem(name)
 	elseif v == "launcher" then
-		newDataObject(nil, name, dobj)
+		newDataObject(name, dobj)
 	end
 end)
 DataRegistry.RegisterCallback(menucontainer, "DataRegistry_DataObjectDestroyed", function(event, name)
--- Panel.lua
+++ Panel.lua
@@ -4,10 +4,11 @@ do
 	setfenv(1, setmetatable(namespace, { __index = _G }))
 end
 
+PANELSIZE = 20
 
 panel = CreateFrame("Frame", nil, UIParent)
-panel:SetHeight(20)
-panel:SetPoint("BOTTOMLEFT", 0, 6)
-panel:SetPoint("BOTTOMRIGHT", 0, 6)
+panel:SetHeight(PANELSIZE)
+panel:SetPoint("BOTTOMLEFT", 0, PANELSIZE / 2)
+panel:SetPoint("BOTTOMRIGHT", 0, PANELSIZE / 2)
 panel:SetBackdrop({ bgFile = [[Interface\BUTTONS\White8x8]] })
 panel:SetBackdropColor(0, 0, 0, 0.8)
